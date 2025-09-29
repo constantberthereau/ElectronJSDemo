3e1c9d205f58edfdb4b2cd8725245027c91d2b92
Constant Berthereau <constant.berthereau@gmail.com>
2025-09-29 09:32:14 +0200

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-link to="/">page One</router-link><br>
+  <router-link to="/page-2">page Two</router-link>
+  <router-view />
+</template>
+<script setup lang="ts">
+</script>
---
+++ src/views/pageOne.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <h1>PageOne</h1>
+  <button @click="callApi" class="uk-button uk-button-primary">Call Api</button>/
+
+  <div v-for="movie in movies">
+    {{movie.title}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+import {ref} from 'vue';
+const movies = ref([]);
+
+async function callApi(){
+  console.log("cc");
+  const res = await fetch('https://raw.githubusercontent.com/Chocolaterie/EniWebService/main/api/movies.json');
+  const data = await res.json();
+  movies.value = data;
+}
+</script>
